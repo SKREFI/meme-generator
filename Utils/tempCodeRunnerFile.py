83cf1c815c6fc04cf566f5237c34ec51199b9f23
SKREFI <srazvan43@yahoo.com>
2020-03-13 22:36:22 +0200

    Setup for Heroku

--- Utils/tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-        max_x = (500 - len(quote)//2 * 18)
-        im = cls.place_text(im, quote, max_x)
-        im.show()
--- app.py
+++ app.py
@@ -78,3 +78,4 @@ def meme_post():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
